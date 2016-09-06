df136335c3d8ff5e912ec80766b853c340f95c17
surbhimanurkar <surbhi.manurkar@gmail.com>
2016-09-06 17:20:05 +0530

    Social with shares and loves by userkey

--- app/src/main/java/com/askoliv/model/Social.java
+++ app/src/main/java/com/askoliv/model/Social.java
@@ -1,13 +1,15 @@
 package com.askoliv.model;
 
+import java.util.Map;
+
 /**
  * Created by surbhimanurkar on 20-06-2016.
  */
 public class Social {
-    int shares;
-    int loves;
+    Map<String,Integer> shares;
+    Map<String,Boolean> loves;
 
-    public Social(int shares, int loves) {
+    public Social(Map<String,Integer> shares, Map<String,Boolean> loves) {
         this.shares = shares;
         this.loves = loves;
     }
@@ -15,19 +17,19 @@ public class Social {
     public Social() {
     }
 
-    public int getShares() {
+    public Map<String,Integer> getShares() {
         return shares;
     }
 
-    public void setShares(int shares) {
+    public void setShares(Map<String,Integer> shares) {
         this.shares = shares;
     }
 
-    public int getLoves() {
+    public Map<String,Boolean> getLoves() {
         return loves;
     }
 
-    public void setLoves(int loves) {
+    public void setLoves(Map<String,Boolean> loves) {
         this.loves = loves;
     }
 }
